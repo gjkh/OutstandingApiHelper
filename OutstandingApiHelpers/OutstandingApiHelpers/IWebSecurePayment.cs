0c2ef4b0db79181697d660eeb7efb8afe0c710ed
gjkh <56520665+gjkh@users.noreply.github.com>
2020-04-02 10:59:28 +0100

    Added IWebSecurePayment for 3DS payments

---
+++ OutstandingApiHelpers/OutstandingApiHelpers/IWebSecurePayment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutstandingApiHelpers
+{
+    public interface IWebSecurePayment
+    {
+        string CardNumber { get; set; }
+        int ExpiryMonth { get; set; }
+        int ExpiryYear { get; set; }
+        string SecurityCode { get; set; }
+        string CardName { get; set; }
+        string CardPostCode { get; set; }
+        decimal PaymentAmount { get; set; }
+
+    }
+}
